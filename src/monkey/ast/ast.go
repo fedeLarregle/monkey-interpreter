64beebe80b451291c9a342ee3985b587050dc552
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2018-08-05 17:32:52 -0300

    AST for let statements. Added parser and parser_test

---
+++ src/monkey/ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type LetStatement struct {
+	Token token.Token // The token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+type Identifier struct {
+	Token token.Token // The token.IDENTIFIER token
+	Value string
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
---
+++ src/monkey/parser/parser.go
@@ -0,0 +1,104 @@
+package parser
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+
+	errors []string
+}
+
+// New function to initialize the Parser
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l, errors: []string{}}
+
+	// Initialize 'currentToken' and 'peekToken' by reading two tokens
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("Expected next token to be %s, got=%s instead", t, p.currentToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.currentTokenIs(token.EOF) {
+		statement := p.parseStatement()
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{Token: p.currentToken}
+
+	if !p.expectPeek(token.IDENTIFIER) {
+		return nil
+	}
+
+	statement.Name = &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: Skipping expressions for now
+	for !p.currentTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return statement
+}
+
+func (p *Parser) currentTokenIs(t token.TokenType) bool {
+	return p.currentToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
